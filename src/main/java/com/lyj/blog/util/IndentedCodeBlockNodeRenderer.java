c790749928cf419a3786d7232a3beb973b2fff2b
shiniaxiaomi <lyj.8066@qqcom>
2019-12-01 01:41:44 +0800

    测试

--- src/main/java/com/lyj/blog/util/IndentedCodeBlockNodeRenderer.java
+++ src/main/java/com/lyj/blog/util/IndentedCodeBlockNodeRenderer.java
@@ -1,5 +1,7 @@
 package com.lyj.blog.util;
 
+import org.commonmark.node.Code;
+import org.commonmark.node.FencedCodeBlock;
 import org.commonmark.node.IndentedCodeBlock;
 import org.commonmark.node.Node;
 import org.commonmark.renderer.NodeRenderer;
@@ -20,16 +22,16 @@ class IndentedCodeBlockNodeRenderer implements NodeRenderer {
     @Override
     public Set<Class<? extends Node>> getNodeTypes() {
         // Return the node types we want to use this renderer for.
-        return Collections.<Class<? extends Node>>singleton(IndentedCodeBlock.class);
+        return Collections.<Class<? extends Node>>singleton(FencedCodeBlock.class);
     }
 
     @Override
     public void render(Node node) {
         // We only handle one type as per getNodeTypes, so we can just cast it here.
-        IndentedCodeBlock codeBlock = (IndentedCodeBlock) node;
+        FencedCodeBlock codeBlock = (FencedCodeBlock) node;
         html.line();
         html.tag("pre");
-        html.text(codeBlock.getLiteral());
+        html.text("1111"+codeBlock.getLiteral()+"2222");
         html.tag("/pre");
         html.line();
     }
