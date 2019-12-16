02bc392c5f216d679796459236462a1f3ef6a459
shiniaxiaomi <lyj.8066@qqcom>
2019-12-16 23:42:03 +0800

    test

--- run.sh
+++ run.sh
@@ -5,16 +5,16 @@ app=$(pgrep -f $appName)
 if test ${app}null != "null"
 then
     # 调用应用接口执行保存操作
-    status=$(curl http://localhost:8080/writeBlogVisitTimes)
-    if test ${status} == 'success'
-    then
-        # 如果保存成功，则停止应用
-        kill -9 $app
-    else
-        # 如果保存失败，停止脚本
-        echo 'blog访问次数保存失败'
-        exit
-    fi
+#    status=$(curl http://localhost:8080/writeBlogVisitTimes)
+#    if test ${status} == 'success'
+#    then
+#        # 如果保存成功，则停止应用
+#        kill -9 $app
+#    else
+#        # 如果保存失败，停止脚本
+#        echo '接口调用失败'
+#        exit
+#    fi
 fi
 
 # 启动应用
