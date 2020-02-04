d673d0a65d2dfd4d76543b204f81aafe8f2bb4d5
shiniaxiaomi <lyj.8066@qqcom>
2020-02-04 11:34:52 +0800

    第四版博客:修改发布脚本

--- run.sh
+++ run.sh
@@ -4,25 +4,7 @@ appName="blog-java"
 app=$(pgrep -f java)
 if test ${app}null != "null"
 then
-    # 是否保存blog访问次数
-    flag=true
-
-    if [ $flag != "true" ]
-    then
-        kill -9 $app
-    else
-        status=$(curl http://localhost/writeBlogVisitTimes)
-        if (( $status == 'success' ))
-        then
-            # 如果保存成功，则停止应用
-            kill -9 $app
-        else
-            # 如果保存失败，停止脚本
-            echo '接口调用失败'
-            exit 0
-        fi
-
-    fi
+    kill -9 $app
 fi
 
 # 启动应用
